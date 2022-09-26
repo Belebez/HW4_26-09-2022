85a15f53889aee5fafa61a5568de6c5a8a49169e
Viktor <viktor.belebezyev@yandex.ru>
2022-09-26 22:38:47 +0300

    add solution algorithm

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,3 +3,38 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+
+
+// Console.Write("Введите число - ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите степень - ");
+// int B = int.Parse(Console.ReadLine()!);
+
+int A = new Random().Next(1,10);
+int B = new Random().Next(1,10);
+Console.WriteLine($"Полученное число - {A}");
+Console.WriteLine($"Полученная степень - {B}");
+
+
+
+int res = forPow(A, B);
+Console.WriteLine($"Число {A} в стпени {B} равно {res}");
+
+int forPow(int num, int pow)
+{
+    int total = 1;
+    for (int i = 1; i <= pow; i++)
+    {
+        total*= num;
+    }
+    return total;
+}
+
+
+int result = numInPow(A, B);
+Console.WriteLine($"Число {A} в стпени {B} равно {result}");
+
+int numInPow(int num, int pow)
+{
+    return (int)Math.Pow(num, pow);
+}
