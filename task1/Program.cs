6fa9ee117541c91ab0b74fe6d227a50fb60ec322
Viktor <viktor.belebezyev@yandex.ru>
2022-09-27 22:58:17 +0300

    edit algorithm

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,37 +4,53 @@
 
 // 2, 4 -> 16
 
+Console.WriteLine("Программа возводит число A в натуральную степень B");
+Console.WriteLine();
+Console.Write("Выберите способ решения, введите 1 или 2 --> ");
+int solutionOption = int.Parse(Console.ReadLine()!);
 
-// Console.Write("Введите число - ");
-// int A = int.Parse(Console.ReadLine()!);
-// Console.Write("Введите степень - ");
-// int B = int.Parse(Console.ReadLine()!);
-
-int A = new Random().Next(1,10);
-int B = new Random().Next(1,10);
-Console.WriteLine($"Полученное число - {A}");
-Console.WriteLine($"Полученная степень - {B}");
-
-
-
-int res = forPow(A, B);
-Console.WriteLine($"Число {A} в стпени {B} равно {res}");
-
-int forPow(int num, int pow)
+if (solutionOption == 1)
 {
-    int total = 1;
-    for (int i = 1; i <= pow; i++)
+    Console.WriteLine("Решение через цикл for. С ручным вводом цифр.");
+    Console.WriteLine();
+    Console.Write("Введите число - ");
+    int A = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите степень - ");
+    int B = int.Parse(Console.ReadLine()!);
+
+    int res = forPow(A, B);
+    Console.WriteLine($"Число {A} в степени {B} равно {res}");
+
+    int forPow(int num, int pow)
     {
-        total*= num;
+        int total = 1;
+        for (int i = 1; i <= pow; i++)
+        {
+            total*= num;
+        }
+        return total;
     }
-    return total;
 }
 
-
-int result = numInPow(A, B);
-Console.WriteLine($"Число {A} в стпени {B} равно {result}");
-
-int numInPow(int num, int pow)
+else if (solutionOption == 2)
 {
-    return (int)Math.Pow(num, pow);
+    Console.WriteLine("Решение через Math.Pow(). Цифры получаем случайным образом.");
+    Console.WriteLine();
+    int A = new Random().Next(1,10);
+    int B = new Random().Next(1,10);
+    Console.WriteLine($"Полученное число - {A}");
+    Console.WriteLine($"Полученная степень - {B}");
+
+    int result = numInPow(A, B);
+    Console.WriteLine($"Число {A} в степени {B} равно {result}");
+
+    int numInPow(int num, int pow)
+    {
+        return (int)Math.Pow(num, pow);
+    }
+}
+
+else
+{
+    Console.WriteLine("Вы ввели отличную от 1 или 2 цифру");
 }
