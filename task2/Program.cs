d73eb6a018cfb2f9602ac755ab99ca786d07ae45
Viktor <viktor.belebezyev@yandex.ru>
2022-09-28 19:32:45 +0300

    add new solution algorithm

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,25 +5,56 @@
 // 82 -> 10
 
 // 9012 -> 12
+Console.Write("Выберите способ решения 1 - через остаток, 2 - через индекс строки: ");
+int way = int.Parse(Console.ReadLine()!);
 
-
-Console.WriteLine("Программа, которая принимает на вход число и выдаёт сумму цифр в числе.");
-Console.WriteLine();
-Console.Write("Введите число - ");
-int A = int.Parse(Console.ReadLine()!);
-
-int res = sumNum(A);
-Console.WriteLine($"Сумма цифр числа {A} равна {res}");
-
-int sumNum(int num)
+if (way == 1)
 {
-    int total = 0;
-    int len = (int)Math.Log10(num) + 1;
-    for (int i = 1; i <= len; i++)
+    Console.WriteLine("Программа, которая принимает на вход число и выдаёт сумму цифр в числе.");
+    Console.WriteLine();
+    Console.Write("Введите число - ");
+    int A = int.Parse(Console.ReadLine()!);
+
+    int res = sumNum(A);
+    Console.WriteLine($"Сумма цифр числа {A} равна {res}");
+
+    int sumNum(int num)
     {
-        int lastNum = num % 10;
-        total += lastNum;
-        num /= 10;
+        int total = 0;
+        int len = (int)Math.Log10(num) + 1;
+        for (int i = 1; i <= len; i++)
+        {
+            int lastNum = num % 10;
+            total += lastNum;
+            num /= 10;
+        }
+        return total;
     }
-    return total;
+}
+else if (way == 2)
+{
+    Console.WriteLine("Программа, которая принимает на вход число и выдаёт сумму цифр в числе.");
+    Console.WriteLine();
+    Console.Write("Введите число - ");
+    string A = Console.ReadLine()!;
+
+    int res = sumNum(A);
+    Console.WriteLine($"Сумма цифр числа {A} равна {res}");
+
+    int sumNum(string num)
+    {
+        int total = 0;
+        int len = num.Length;
+        for (int i = 0; i < len; i++)
+        {
+            int x = (int)char.GetNumericValue(num[i]);  // преобразование символа в число
+            total += x;
+        }
+        return total;
+    }
+}
+
+else
+{
+    Console.WriteLine("Вы ввели отличное от 1 или 2 цифру.");
 }
